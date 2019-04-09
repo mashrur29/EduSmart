7bfe4beeba1c26f1b617dbdc24d6aee9e4c454d3
mashrur29 <mashrur639@gmail.com>
2019-04-09 13:13:21 +0600

    Tutor, Profile and Online Judge Implemented

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import ast
-
+import requests
 from bson import ObjectId, Binary
-from flask import Flask, render_template, flash, redirect, url_for, session, request, logging, session
-import sqlite3
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging, session, json
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from functools import wraps
 from database import db
@@ -111,17 +110,11 @@ class RegisterForm(Form):
 def register():
     form = RegisterForm(request.form)
     if request.method == 'POST' and form.validate():
-        conn = sqlite3.connect('tutorial.db')
-        c = conn.cursor()
         name = form.name.data
         email = form.email.data
         username = form.username.data
         password = form.password.data
-        c.execute("INSERT INTO users (name, Username, Email, password) VALUES (?, ?, ?, ?)",
-                  (name, username, email, password))
-        conn.commit()
-        c.close()
-        conn.close()
+        db.users.insert({"name": name, "email": email, "username": username, "password": password})
         flash('You are now registered and can log in', 'success')
         return redirect(url_for('index'))
     return render_template('register.html', form=form)
@@ -135,35 +128,30 @@ def login():
         # getting form fields
         Username = request.form['username']
         password_candidate = request.form['password']
-        conn = sqlite3.connect('tutorial.db')
-        c = conn.cursor()
+        users = db.users.find({"username": str(Username)})
 
-        c.execute("SELECT * FROM users WHERE Username = ?", (Username,))
-        data = c.fetchone()
-        # print(data)
 
-        c.close()
-        conn.close()
+        for user in users:
+            if user is not None:
+                # Get stored hash
+                password = str(user["password"])
 
-        if data is not None:
-            # Get stored hash
-            password = data[3]
-            print('In')
+                print('In')
 
-            # Compare Passwords
-            if password_candidate == password:
-                # Passed
-                session['logged_in'] = True
-                session['username'] = Username
-                flash('You are now logged in', 'success')
-                return redirect(url_for('dashboard'))
-            else:
-                error = 'Invalid login'
-                return render_template('login.html', error=error)
+                # Compare Passwords
+                if password_candidate == password:
+                    # Passed
+                    session['logged_in'] = True
+                    session['username'] = Username
+                    flash('You are now logged in', 'success')
+                    return redirect(url_for('dashboard'))
+                else:
+                    error = 'Invalid login'
+                    return render_template('login.html', error=error)
 
-        else:
-            error = 'Username not found'
-            return render_template('login.html', error=error)
+
+        error = 'Username not found'
+        return render_template('login.html', error=error)
 
     return render_template('login.html')
 
@@ -209,9 +197,20 @@ def add_article():
 
 #######################################################################################
 
+@app.route('/download_file/<string:id>', methods=['POST', 'GET'])
+def download_file(id):
+    print('Downloading')
+    file = db.projects.find({"_id": ObjectId(id)})
+    f = open('download', 'w')
+    str1 = 'download'
+    f.write(str1)
+    f.close()
+    print('file downloaded')
+
+    return redirect(url_for('dashboard'))
+
 @app.route('/upload_file/<string:id>', methods=['POST', 'GET'])
 def upload_file(id):
-    print('lol', id)
     file = request.files['inputfile']
     author = session['username']
     title = file.filename
@@ -344,6 +343,197 @@ def delete_article(id):
 
 
 #########################################################################################
+
+@app.route('/jobs')
+def jobs():
+    jobs = db.jobs.find()
+
+    if jobs is not None:
+        return render_template('jobs.html', jobs=jobs)
+    else:
+        msg = 'Nobody is Interested :('
+        return render_template('jobs.html', msg=msg)
+
+@app.route('/delete_job/<string:id>', methods=['GET', 'POST'])
+def delete_job(id):
+    db.jobs.remove({"_id": ObjectId(id)})
+    flash('Job Deleted', 'success')
+    return redirect(url_for('jobs'))
+
+class JobForm(Form):
+    tutor = StringField('Tutor', [validators.Length(min=1, max=5000)])
+    contact = StringField('Contact', [validators.Length(min=1, max=5000)])
+    subject = StringField('Interested Subject', [validators.Length(min=1, max=5000)])
+    compensation = StringField('Compensation', [validators.Length(min=1, max=5000)])
+
+@app.route('/add_job', methods=['GET', 'POST'])
+@is_logged_in
+def add_job():
+    form = JobForm(request.form)
+    if request.method == 'POST' and form.validate():
+        contact = form.contact.data
+        subject = form.subject.data
+        compensation = form.compensation.data
+        tutor = form.tutor.data
+
+        db.jobs.insert({"contact": contact, "subject": subject, "tutor": tutor, "compensation": compensation})
+
+        flash('Job Created', 'success')
+
+        return redirect(url_for('jobs'))
+
+    return render_template('add_job.html', form=form)
+
+
+
+
+##########################################################################################
+
+languages = {"c": 1, "cpp": 2, "cpp14": 58, "java": 3,"mysql": 10,"python2": 5, "python3": 30,"text": 28}
+
+@app.route('/online_judge', methods=['GET', 'POST'])
+def online_judge():
+    problems = db.problems.find()
+
+    if problems is not None:
+        return render_template('online_judge.html', problems=problems)
+    else:
+        msg = 'No Problems Available'
+        return render_template('online_judge.html', msg=msg)
+
+class ProblemForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=5000)])
+    body = TextAreaField('Body', [validators.Length(min=1)])
+    input = TextAreaField('Input', [validators.Length(min=0)])
+    output = TextAreaField('Output', [validators.Length(min=1)])
+
+@app.route('/add_problem', methods=['GET', 'POST'])
+def add_problem():
+    form = ProblemForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        body = form.body.data
+        input = form.input.data
+        output = form.output.data
+
+        db.problems.insert({"name": name, "body": body, "input": input, "output": output})
+
+        flash('Problem Created', 'success')
+
+        return redirect(url_for('online_judge'))
+
+    return render_template('add_problem.html', form=form)
+
+@app.route('/problem/<string:id>/')
+def problem(id):
+    problem = db.problems.find_one({"_id": ObjectId(id)})
+
+    print(problem['name'])
+    return render_template('problem.html', problem=problem)
+
+class SolveProblemForm(Form):
+    code = TextAreaField('Source Code', [validators.Length(min=1)])
+    lang = StringField('Source Language', [validators.Length(min=1)])
+
+
+@app.route('/add_problem/<string:id>/', methods=['GET', 'POST'])
+def solve_problem(id):
+    form = SolveProblemForm(request.form)
+    problem = db.problems.find_one({"_id": ObjectId(id)})
+    if request.method == 'POST' and form.validate():
+        code = form.code.data
+        language = form.lang.data
+        input_ = problem['input']
+        if input_ == "":
+            input_ = " "
+
+        verdict = 'Wrong Answer'
+        guest = hackerrank_api(code=code, language=language, input_=input_)
+        print(guest['output'])
+        if guest['result'] == "Successfully Executed":
+            if(guest['output'].rstrip() == problem['output']):
+                verdict = 'Accepted'
+                flash('Accepted', 'success')
+            else:
+                flash('Wrong Answer', 'danger')
+        else:
+            flash('Compilation Error', 'warning')
+
+        db.submission.insert({"name": problem['name'], "user": session['username'], "verdict": verdict})
+        return redirect(url_for('online_judge'))
+
+    return render_template('solve_problem.html', form=form, languages=languages)
+
+
+def hackerrank_api(username=None, title=None, code=None, language=None, input_=None):
+    language = int(language)
+    s_lang = (list(languages.keys())[list(languages.values()).index(language)])
+    print(s_lang)
+    try:
+        RUN_URL = 'https://api.jdoodle.com/v1/execute'
+        headers = {'content-type': 'application/json'}
+        data = {
+            'script': code,
+            'language': s_lang,
+            'stdin': input_,
+            'versionIndex': '0',
+            'clientId': '5cd01283205107ec2f32a6380c097804',
+            'clientSecret': '9c7bb43dab2f76c610a7ed85870d00d20754e9717a0e09bd3d1fc9cbd457165a',
+
+        }
+
+        r = requests.post(RUN_URL, data=json.dumps(data), headers=headers)
+        response = r.json()
+        output = response['output']
+        result = "Compilation Error"
+        time = None
+        mem = None
+        print(response)
+        if output:
+            message = response['statusCode']
+            if message == 200:
+                result = "Successfully Executed"
+                output = response['output']
+                time = response['cpuTime']
+                mem = response['memory']
+            elif message != "200":
+                result = "Runtime Error"
+                output = response['error']
+    except Exception as e:
+        print(e)
+        result = "Unable to process your request. Please try again later. Sorry for inconvenience."
+        output = None
+        time = None
+        mem = None
+    O_IDE = {}
+    O_IDE['code'] = code
+    O_IDE['lang'] = int(language)
+    O_IDE['input'] = input_
+    O_IDE['output'] = output
+    O_IDE['result'] = result
+    O_IDE['time'] = time
+    O_IDE['memory'] = mem
+    if username is not None:
+        # Authenticated User
+        O_IDE['username'] = username
+        O_IDE['title'] = title
+    # print(O_IDE)
+    return O_IDE
+
+##########################################################################################
+
+@app.route('/profile')
+def profile():
+    users = db.users.find({"username": str(session['username'])})
+    curuser = None
+    for user in users:
+        curuser = user
+        break
+    submissions = db.submission.find({"user": str(session['username'])})
+    return render_template('profile.html', user=curuser, submissions=submissions)
+
+
+##########################################################################################
 
 if __name__ == '__main__':
     app.secret_key = 'super secret key'
